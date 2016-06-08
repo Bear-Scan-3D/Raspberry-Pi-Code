f820a6d40888bacec240c4e567fc0c4f6cdf6ccd
Bear-Scan-3D <beartechandtools@gmail.com>
2016-06-08 21:42:44 +0200

    print

--- pystepp.py
+++ pystepp.py
@@ -8,7 +8,7 @@ try:
     steps = int(float(sys.argv[2]))
 except:
     steps = 0
-	print ("Keine Perimeter angegeben. Bitte angeben: programmname.py Richtung Schritte Speed")
+	print ("Keine Perimeter angegeben.")
 
 #Kontrollausgabe der Eingegebenen Perimeter
 print("Bewege den Motor %s Schritte nach %s.") % (steps, direction)
