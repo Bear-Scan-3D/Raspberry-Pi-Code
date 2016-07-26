49408cc6b9c175dcccf957143143a81788793f41
Bear-Scan-3D <beartechandtools@gmail.com>
2016-07-26 14:24:22 +0200

    Displaxy test

--- sevensegment_test_modified.py
+++ sevensegment_test_modified.py
@@ -13,24 +13,17 @@ print 'Beginne Testprogramm'
 
 # Run through different number printing examples.
 print('Press Ctrl-C to quit.')
-number = 10
+
 while True:
     # Print floating point values with default 2 digit precision.
     # Clear the display buffer.
     display.clear()
     # Print a floating point number to the display.
     #display.print_number_str(number)
-    number += 1
     #display.write_display()
     # Delay for a second.
     time.sleep(1.0)
 
-    display.print_float(0.10)
-    display.set_colon(False)
-    display.set_brightness(1)
-    display.write_display()
-
-    time.sleep(1.0)
 
     display.print_number_str('A23F')
     display.set_colon(False)
@@ -40,14 +33,12 @@ while True:
     i = 0
     while i < 10:
 
-        display.set_brightness(15)
-        time.sleep(0.2)
         display.set_brightness(1)
-        time.sleep(0.2)
+        time.sleep(0.1)
+        display.set_brightness(15)
+        time.sleep(0.1)
 
         i +=1
-
-
 
     #display.set_invert(False)
     # Make sure to call write_display() to make the above visible!
