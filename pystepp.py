65bf64fcb9288f02269f199238e0d8de5cecb46c
Bear-Scan-3D <beartechandtools@gmail.com>
2016-09-02 20:08:44 +0200

    Accel Test

--- pystepp.py
+++ pystepp.py
@@ -29,8 +29,8 @@ gpio.setup(24, gpio.OUT) #Step
 gpio.setup(25, gpio.OUT) #enable pin
 
 #GPIO for using the switch of the rotary encoder seperately
-gpio.setup(4, gpio.IN, pull_up_down = gpio.PUD_DOWN)
-
+#gpio.setup(4, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+gpio.setup(4, gpio.OUT)
 #make instance of display
 display = SevenSegment.SevenSegment()
 
