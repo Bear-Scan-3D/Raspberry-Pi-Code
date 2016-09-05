57d4458c706c8984959c69f83c295e321921374e
Bear-Scan-3D <beartechandtools@gmail.com>
2016-09-05 13:36:33 +0200

    SpeedTest

--- pystepp.py
+++ pystepp.py
@@ -60,7 +60,7 @@ def moveStepper(steps):#moves the motor a certain amount ('steps')
     stepCounter = 0
     maxSpeed = 0.0015
     jerkSpeed = 0.01
-    acceleration = 0.98
+    acceleration = 0.97
 
     brakeThreshold = 100
     if steps < (brakeThreshold*2):
