86421b78cca4c945eed1c0b5192ebaccee3aac34
Bear-Scan-3D <beartechandtools@gmail.com>
2016-09-05 13:34:25 +0200

    SpeedTest

--- pystepp.py
+++ pystepp.py
@@ -58,7 +58,7 @@ camera = picamera.PiCamera()
 
 def moveStepper(steps):#moves the motor a certain amount ('steps')
     stepCounter = 0
-    maxSpeed = 0.001
+    maxSpeed = 0.0015
     jerkSpeed = 0.01
     acceleration = 0.98
 
